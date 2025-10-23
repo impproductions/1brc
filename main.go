12f51fd7cc1043a6e904023983a126a23026f749
Luca Paolella <luca.paolella@gmail.com>
2025-10-24 00:49:55 +0200

    refactoring, optimization

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"io"
 	"os"
-	"runtime/pprof"
 	"sort"
 	"sync"
 	"time"
@@ -19,7 +18,16 @@ type Stat struct {
 	Max   float64
 }
 
-var stats map[string]*Stat = make(map[string]*Stat, 500)
+const (
+	CITY        = 0
+	TEMPERATURE = 1
+)
+
+const chunkSize = 16777216
+const parallelism = 8
+const filePath = "assets/measurements_max.txt"
+
+var stats map[string]*Stat = make(map[string]*Stat, 10000)
 
 func ProcessChunks(f *os.File, chunkSize int, parallelism int64) {
 	ctx, canc := context.WithCancel(context.Background())
@@ -30,26 +38,26 @@ func ProcessChunks(f *os.File, chunkSize int, parallelism int64) {
 		panic("couldn't retreive file stats")
 	}
 
-	var wgProd sync.WaitGroup
-	var wgRec sync.WaitGroup
-
 	permit := struct{}{}
 	semaphore := make(chan struct{}, parallelism)
 
+	var wgProd sync.WaitGroup
+	var wgRec sync.WaitGroup
+
 	var size int64 = info.Size()
 	var offset int64
-
 	updates := make(chan map[string]*Stat)
+
 	wgRec.Add(1)
 	go func() {
 		defer wgRec.Done()
 		for statsUpdate := range updates {
 			for k, updatedVal := range statsUpdate {
 				if val, exists := stats[k]; exists {
-					stats[k].Min = min(val.Min, updatedVal.Min)
-					stats[k].Tot = val.Tot + updatedVal.Tot
-					stats[k].Count = val.Count + updatedVal.Count
-					stats[k].Max = max(val.Max, updatedVal.Max)
+					val.Min = min(val.Min, updatedVal.Min)
+					val.Tot = val.Tot + updatedVal.Tot
+					val.Count = val.Count + updatedVal.Count
+					val.Max = max(val.Max, updatedVal.Max)
 				} else {
 					stats[k] = updatedVal
 				}
@@ -95,60 +103,50 @@ func ProcessChunk(r io.ReaderAt, from int64, bytes int) (map[string]*Stat, error
 	buf := make([]byte, overshotLength)
 
 	read, err := r.ReadAt(buf, from)
-	if err != nil && err != io.EOF {
-		panic(err)
+	if err != nil {
+		if err != io.EOF {
+			panic(err)
+		} else if read == 0 {
+			return nil, err
+		}
 	}
 
-	if read == 0 && err == io.EOF {
-		return nil, err
+	start := 0
+	for ; buf[start] != '\n' && from > 0; start++ {
+	}
+	if from == 0 {
+		start = -1
 	}
 
 	limit := min(read, bytes)
+	localStats := make(map[string]*Stat, 10000) // 10k possible cities
+	starts := make([]int, 2)
+	ends := make([]int, 2)
+	// city cursors
+	starts[CITY] = start + 1
+	ends[CITY] = starts[CITY]
+	// temperature cursors
+	starts[TEMPERATURE] = 0
+	ends[TEMPERATURE] = 0
+	mode := CITY
 
-	localStats := make(map[string]*Stat, 500)
+	for i := starts[CITY]; i < overshotLength && (i <= limit || buf[i-1] != '\n'); i++ {
+		c := buf[i]
 
-	cityStart, cityEnd := 0, 0
-	tempStart, tempEnd := 0, 0
-	leftOfSemicolon := true
-	isFirstLine := true
-	if from == 0 {
-		isFirstLine = false
-	}
-	for p := range overshotLength {
-		c := buf[p]
-		if isFirstLine {
-			if c == '\n' {
-				isFirstLine = false
-				cityStart = p + 1
-				cityEnd = p + 1
-			}
-			continue
-		}
-		if p > limit {
-			if buf[p-1] == '\n' {
-				// we've overshot and are done processing the last line of the chunk
-				break
-			}
-		}
-		if c == ';' {
-			leftOfSemicolon = false
-			tempStart = p + 1
-			tempEnd = tempStart
-			continue
-		}
-		if c == '\n' && p <= read-1 {
-			unsafeCurrentCityString := unsafeBytesAsString(buf[cityStart:cityEnd])
-			currentTemp := parseFloat(buf[tempStart:tempEnd])
-			leftOfSemicolon = true
+		switch {
+		case c != '\n' && c != ';':
+			ends[mode]++
+		case c == '\n':
+			unsafe_currentCityString := unsafe.String(&buf[starts[CITY]], ends[CITY]-starts[CITY]) // string() just to access the map takes up a huge chunk of time
+			currentTemp := parseFloat(buf[starts[TEMPERATURE]:ends[TEMPERATURE]])
 
-			val, found := localStats[unsafeCurrentCityString]
+			val, found := localStats[unsafe_currentCityString]
 			if !found {
 				val = &Stat{
 					Min: currentTemp,
 					Max: currentTemp,
 				}
-				currentCitySafe := string(buf[cityStart:cityEnd])
-				localStats[currentCitySafe] = val
+				localStats[string(buf[starts[CITY]:ends[CITY]])] = val
 			}
 
 			val.Min = min(val.Min, currentTemp)
@@ -156,32 +154,17 @@ func ProcessChunk(r io.ReaderAt, from int64, bytes int) (map[string]*Stat, error
 			val.Count++
 			val.Max = max(val.Max, currentTemp)
 
-			cityStart = p + 1
-			cityEnd = cityStart
-
-			continue
-		}
-
-		if leftOfSemicolon {
-			cityEnd++
-		} else {
-			tempEnd++
+			mode = CITY
+			starts[CITY] = i + 1
+			ends[CITY] = starts[CITY]
+		default:
+			mode = TEMPERATURE
+			starts[TEMPERATURE] = i + 1
+			ends[TEMPERATURE] = starts[TEMPERATURE]
 		}
 	}
 
 	return localStats, err
-}
-
-// func ceilTo(n float64, decimals int) float64 {
-// 	factor := math.Pow(10, float64(decimals))
-// 	return math.Ceil(n*factor) / factor
-// }
-
-func unsafeBytesAsString(b []byte) string {
-	if len(b) == 0 {
-		return ""
-	}
-	return unsafe.String(&b[0], len(b))
 }
 
 func parseFloat(bytes []byte) float64 {
@@ -205,21 +188,6 @@ func parseFloat(bytes []byte) float64 {
 }
 
 func main() {
-	// CPU profiling
-	cpuProfile, err := os.Create("cpu.prof")
-	if err != nil {
-		panic(err)
-	}
-	defer cpuProfile.Close()
-	if err := pprof.StartCPUProfile(cpuProfile); err != nil {
-		panic(err)
-	}
-	defer pprof.StopCPUProfile()
-
-	const chunkSize = 16777216
-	const parallelism = 8
-	const filePath = "assets/measurements_max.txt"
-
 	start := time.Now()
 	f, err := os.Open(filePath)
 	if err != nil {
@@ -239,10 +207,10 @@ func main() {
 	sort.Strings(keys)
 	for i, k := range keys {
 		v := stats[k]
-		fmt.Printf("%s=%.1f/%.1f/%.1f", k, v.Min, v.Tot/float64(v.Count), v.Max)
-		if i < len(keys)-1 {
+		if i > 0 {
 			fmt.Printf(", ")
 		}
+		fmt.Printf("%s=%.1f/%.1f/%.1f", k, v.Min, v.Tot/float64(v.Count), v.Max)
 	}
 	fmt.Printf("}\n")
 
